c8ad1121d2fbf6378291c46247d89675669f8e5d
João Rodrigues <joao.devmail@gmail.com>
2022-06-09 13:23:36 -0300

    feat: config module installed

---
+++ api/config/default.ts
@@ -0,0 +1,15 @@
+function getEnvironmentVariable(name: string) {
+  const value = process.env[name];
+  if (!value) {
+    throw new Error(`Missing environment variable ${name}`);
+  }
+  return value;
+}
+
+export default {
+  server: {
+    domain: getEnvironmentVariable("DOMAIN"),
+    protocol: getEnvironmentVariable("PROTOCOL"),
+    port: 4000,
+  },
+};
--- api/src/index.ts
+++ api/src/index.ts
@@ -1,10 +1,23 @@
+import config from "config";
 import express from "express";
+import cors from "cors";
+
+const domain: string = config.get("server.domain");
+const protocol: string = config.get("server.protocol");
+const port: number = config.get("server.port");
 
 async function startServer() {
   const app = express();
 
-  app.listen(4000);
-  console.log("🚀 Server ready at http://localhost:4000");
+  // Add CORS middleware
+  app.use(
+    cors({
+      origin: [`${protocol}://${domain}`],
+    })
+  );
+
+  app.listen(port);
+  console.log(`🚀 Server ready at ${protocol}://localhost:${port}`);
 }
 
 startServer();
