0d5a1f7d9fb81b518af838c690966bfccf48a030
João Rodrigues <joao.devmail@gmail.com>
2022-06-17 16:21:08 -0300

    feat: deleteGroup graphql mutation

--- api/src/graphql/mutations/group.ts
+++ api/src/graphql/mutations/group.ts
@@ -1,6 +1,6 @@
 import { UserInputError } from "apollo-server-express";
-import { arg, extendType } from "nexus";
-import { createGroupUseCase } from "../../useCases/group";
+import { arg, extendType, idArg } from "nexus";
+import { createGroupUseCase, deleteGroupUseCase } from "../../useCases/group";
 
 export const groupMutations = extendType({
   type: "Mutation",
@@ -13,6 +13,22 @@ export const groupMutations = extendType({
       async resolve(_root, { data }) {
         try {
           return await createGroupUseCase.execute(data);
+        } catch (err) {
+          if (err instanceof Error) {
+            throw new UserInputError(err.message);
+          }
+          throw err;
+        }
+      },
+    });
+    t.field("deleteGroup", {
+      type: "Group",
+      args: {
+        id: idArg(),
+      },
+      async resolve(_root, args) {
+        try {
+          return await deleteGroupUseCase.execute(args);
         } catch (err) {
           if (err instanceof Error) {
             throw new UserInputError(err.message);
--- api/src/useCases/group/index.ts
+++ api/src/useCases/group/index.ts
@@ -1,6 +1,7 @@
 import { groupRepository, userRepository } from "../../repositories";
 import { AddUserToGroupUseCase } from "./addUserToGroup/add-user-to-group.usecase";
 import { CreateGroupUseCase } from "./createGroup/create-group.usecase";
+import { DeleteGroupUseCase } from "./deleteGroup/delete-group.usecase";
 import { GetGroupUseCase } from "./getGroup/get-group.usecase";
 import { GetGroupsUseCase } from "./getGroups/get-groups.usecase";
 import { RemoveUserFromGroupUseCase } from "./removeUserFromGroup/remove-user-from-group.usecase";
@@ -8,6 +9,7 @@ import { RemoveUserFromGroupUseCase } from "./removeUserFromGroup/remove-user-fr
 const getGroupUseCase = new GetGroupUseCase(groupRepository);
 const getGroupsUseCase = new GetGroupsUseCase(groupRepository);
 const createGroupUseCase = new CreateGroupUseCase(groupRepository);
+const deleteGroupUseCase = new DeleteGroupUseCase(groupRepository);
 const addUserToGroupUseCase = new AddUserToGroupUseCase(
   groupRepository,
   userRepository
@@ -21,6 +23,7 @@ export {
   getGroupUseCase,
   getGroupsUseCase,
   createGroupUseCase,
+  deleteGroupUseCase,
   addUserToGroupUseCase,
   removeUserFromGroupUseCase,
 };
