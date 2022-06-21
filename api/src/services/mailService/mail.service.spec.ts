3131d343e6583efa2e40e4d8c83da57aa192537f
João Rodrigues <joao.devmail@gmail.com>
2022-06-21 13:16:41 -0300

    test: MailService tests

---
+++ api/src/services/mailService/mail.service.spec.ts
@@ -0,0 +1,41 @@
+import { GmailMailProvider } from "../../providers/mail";
+import { MailService } from "./mail.service";
+import { mailTransporterMock } from "../../tests/mock/nodemailer";
+
+function buildSUT(): {
+  mailService: MailService;
+} {
+  const mailProvider = new GmailMailProvider(mailTransporterMock);
+  const mailService = new MailService(mailProvider);
+  return { mailService };
+}
+
+describe("MailService", () => {
+  it("should render html templates", async () => {
+    const { mailService } = buildSUT();
+    const html = await mailService.renderHtmlTemplate("accountActivation", {
+      domain: "test.com",
+      protocol: "http",
+      title: "Testing",
+      to: {
+        name: "John",
+        address: "john@test.com",
+      },
+      url: "testing",
+    });
+    expect(html).toBeTruthy();
+  });
+  it("should send emails", async () => {
+    const { mailService } = buildSUT();
+    await expect(
+      mailService.sendMail({
+        to: { name: "John", address: "john@test.com" },
+        subject: "Testing",
+        template: "accountActivation",
+        data: {
+          url: "testing",
+        },
+      })
+    ).resolves.toBeUndefined();
+  });
+});
