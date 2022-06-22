cd710800d14f16bce64686e1503ba32e65d6fa58
João Rodrigues <joao.devmail@gmail.com>
2022-06-22 11:08:17 -0300

    feat: ResetPasswordUseCase implementation

---
+++ api/src/useCases/auth/resetPassword/reset-password.spec.ts
@@ -0,0 +1,108 @@
+import RedisMock from "ioredis-mock";
+import { ValidationError } from "yup";
+import { InvalidTokenError } from "../../../errors";
+import { JwtRepository } from "../../../repositories/jwt";
+import { UserRepository } from "../../../repositories/user";
+import { JwtService } from "../../../services/jwtService/jwt.service";
+import { createFakeUser } from "../../../tests/fake/user";
+import { prismaMock } from "../../../tests/mock/prisma";
+import { ResetPasswordDto } from "./reset-password.dto";
+import { ResetPasswordUseCase } from "./reset-password.usecase";
+
+function buildSUT(): {
+  resetPasswordUseCase: ResetPasswordUseCase;
+  jwtService: JwtService;
+} {
+  const jwtRepository = new JwtRepository(new RedisMock());
+  const jwtService = new JwtService(jwtRepository);
+  const userRepository = new UserRepository(prismaMock);
+  const resetPasswordUseCase = new ResetPasswordUseCase(
+    jwtService,
+    userRepository
+  );
+  return { resetPasswordUseCase, jwtService };
+}
+
+describe("ResetPassword", () => {
+  it("should validate the password", async () => {
+    const data: ResetPasswordDto = {
+      password: "123456",
+      token: "1234",
+    };
+    const { resetPasswordUseCase } = buildSUT();
+
+    await expect(
+      resetPasswordUseCase.validateResetPasswordData(data)
+    ).resolves.toBeTruthy();
+
+    data.password = "";
+    await expect(
+      resetPasswordUseCase.validateResetPasswordData(data)
+    ).rejects.toThrowError(ValidationError);
+
+    data.password = "1234";
+    data.token = "";
+    await expect(
+      resetPasswordUseCase.validateResetPasswordData(data)
+    ).rejects.toThrowError(ValidationError);
+  });
+  it("should validate the token", async () => {
+    const { resetPasswordUseCase, jwtService } = buildSUT();
+    let token = "1234";
+
+    await expect(resetPasswordUseCase.validateToken(token)).rejects.toThrow(
+      InvalidTokenError
+    );
+
+    token = await jwtService.signToken("resetPasswordToken", { id: "1" }, "1");
+    await expect(
+      resetPasswordUseCase.validateToken(token)
+    ).resolves.toBeTruthy();
+  });
+  it("should check if the user exists", async () => {
+    const { resetPasswordUseCase } = buildSUT();
+    const user = createFakeUser({ id: "1" }, 1);
+
+    prismaMock.user.findUnique.mockResolvedValue(null);
+    await expect(resetPasswordUseCase.getUserById(user.id)).rejects.toThrow(
+      InvalidTokenError
+    );
+
+    prismaMock.user.findUnique.mockResolvedValue(user);
+    await expect(resetPasswordUseCase.getUserById(user.id)).resolves.toBe(user);
+  });
+  it("should hash the new password", async () => {
+    const { resetPasswordUseCase } = buildSUT();
+    const password = "123456";
+
+    const passwordHash = await resetPasswordUseCase.hashPassword(password);
+    expect(passwordHash).toBeTruthy();
+    expect(passwordHash).not.toBe(password);
+  });
+  it("should update the user password", async () => {
+    const { resetPasswordUseCase, jwtService } = buildSUT();
+    const user = createFakeUser({ id: "1" }, 1);
+    const data: ResetPasswordDto = {
+      password: "123456",
+      token: await jwtService.signToken(
+        "resetPasswordToken",
+        { id: user.id },
+        user.id
+      ),
+    };
+
+    prismaMock.user.findUnique.mockResolvedValue(null);
+    await expect(resetPasswordUseCase.execute(data)).rejects.toThrow(
+      InvalidTokenError
+    );
+
+    data.token = await jwtService.signToken(
+      "resetPasswordToken",
+      { id: user.id },
+      user.id
+    );
+    prismaMock.user.findUnique.mockResolvedValue(user);
+    prismaMock.user.update.mockResolvedValue(user);
+    await expect(resetPasswordUseCase.execute(data)).resolves.toBeTruthy();
+  });
+});
---
+++ api/src/useCases/auth/resetPassword/reset-password.usecase.ts
@@ -0,0 +1,68 @@
+import * as yup from "yup";
+import bcrypt from "bcrypt";
+import { InvalidTokenError } from "../../../errors";
+import { IJwtService, IUseCase, IUserRepository } from "../../../interfaces";
+import { ResetPasswordDto } from "./reset-password.dto";
+import { JwtPayload } from "jsonwebtoken";
+import { User } from "@prisma/client";
+
+export class ResetPasswordUseCase implements IUseCase<ResetPasswordDto, true> {
+  constructor(
+    private readonly jwtService: IJwtService,
+    private readonly userRepository: IUserRepository
+  ) {}
+
+  async validateResetPasswordData(
+    data: ResetPasswordDto
+  ): Promise<ResetPasswordDto> {
+    const resetPasswordDataConstraints = yup.object().shape({
+      password: yup.string().required().min(6).max(50),
+      token: yup.string().required(),
+    });
+    return resetPasswordDataConstraints.validate(data);
+  }
+
+  async validateToken(token: string): Promise<false | JwtPayload> {
+    try {
+      return await this.jwtService.validateToken("resetPasswordToken", token);
+    } catch (err) {
+      throw new InvalidTokenError("Invalid token");
+    }
+  }
+
+  async getUserById(id: string): Promise<User> {
+    const user = await this.userRepository.findOne({
+      id,
+    });
+    if (!user) {
+      throw new InvalidTokenError("Invalid refresh token");
+    }
+    return user;
+  }
+
+  async hashPassword(password: string): Promise<string> {
+    return bcrypt.hash(password, 12);
+  }
+
+  async execute(args: ResetPasswordDto): Promise<true> {
+    const validatedData = await this.validateResetPasswordData(args);
+    const payload = await this.validateToken(validatedData.token);
+
+    if (!payload || typeof payload.id !== "string") {
+      throw new InvalidTokenError("Invalid refresh token");
+    }
+    const user = await this.userRepository.findOne({
+      id: payload.id,
+    });
+    if (!user) {
+      throw new InvalidTokenError("Invalid refresh token");
+    }
+
+    const passwordHash = await this.hashPassword(validatedData.password);
+    await this.userRepository.update(
+      { id: user.id },
+      { password: passwordHash }
+    );
+    return true;
+  }
+}
