87af6ec84b26952b15a0b199ac6c576ffac95acb
João Rodrigues <joao.devmail@gmail.com>
2022-06-27 12:17:15 -0300

    feat: CreateUserUseCase implementation

---
+++ api/src/useCases/user/createUser/create-user.spec.ts
@@ -0,0 +1,102 @@
+import { ValidationError } from "yup";
+import { UserRepository } from "../../../repositories/user";
+import { prismaMock } from "../../../tests/mock/prisma";
+import { CreateUserDto } from "./create-user.dto";
+import { CreateUserUseCase } from "./create-user.usecase";
+import { createFakeUser } from "../../../tests/fake/user";
+import { IntegrityError } from "../../../errors";
+
+function buildSUT(): {
+  createUserUseCase: CreateUserUseCase;
+} {
+  const userRepository = new UserRepository(prismaMock);
+  const createUserUseCase = new CreateUserUseCase(userRepository);
+  return { createUserUseCase };
+}
+
+describe("CreateUserUseCase", () => {
+  it("should check if all fields are within assigned constraints", async () => {
+    const data: CreateUserDto = {
+      firstName: "John",
+      lastName: "Doe",
+      email: "john@test.com",
+      password: "123456",
+      isActive: true,
+      isVerified: true,
+    };
+    let testData = { ...data };
+    const { createUserUseCase } = buildSUT();
+
+    await expect(
+      createUserUseCase.validateCreateUserData(testData)
+    ).resolves.toBeTruthy();
+
+    testData.firstName = "";
+    await expect(
+      createUserUseCase.validateCreateUserData(testData)
+    ).rejects.toThrow(ValidationError);
+
+    testData = { ...data };
+    testData.lastName = "do";
+    await expect(
+      createUserUseCase.validateCreateUserData(testData)
+    ).rejects.toThrow(ValidationError);
+
+    testData = { ...data };
+    testData.email = "test.com";
+    await expect(
+      createUserUseCase.validateCreateUserData(testData)
+    ).rejects.toThrow(ValidationError);
+
+    testData = { ...data };
+    testData.password = "12";
+    await expect(
+      createUserUseCase.validateCreateUserData(testData)
+    ).rejects.toThrow(ValidationError);
+  });
+  it("should check if unique fields are not in user", async () => {
+    const data: CreateUserDto = {
+      firstName: "John",
+      lastName: "Doe",
+      email: "john@test.com",
+      password: "123456",
+      isActive: true,
+      isVerified: true,
+    };
+    const users = [createFakeUser({ email: data.email }, 1)];
+    const { createUserUseCase } = buildSUT();
+
+    prismaMock.user.findMany.mockResolvedValue(users);
+    await expect(createUserUseCase.checkUserUniqueFields(data)).rejects.toThrow(
+      IntegrityError
+    );
+
+    prismaMock.user.findMany.mockResolvedValue([]);
+    await expect(
+      createUserUseCase.checkUserUniqueFields(data)
+    ).resolves.toBeTruthy();
+  });
+  it("should hash password", async () => {
+    const password = "123456";
+    const { createUserUseCase } = buildSUT();
+
+    const hashedPassword = await createUserUseCase.hashPassword(password);
+    expect(hashedPassword).toBeTruthy();
+    expect(hashedPassword).not.toBe(password);
+  });
+  it("should create the user", async () => {
+    const data: CreateUserDto = {
+      firstName: "John",
+      lastName: "Doe",
+      email: "john@test.com",
+      password: "123456",
+      isActive: true,
+      isVerified: true,
+    };
+    const { createUserUseCase } = buildSUT();
+
+    prismaMock.user.findMany.mockResolvedValue([]);
+    prismaMock.user.create.mockResolvedValue(createFakeUser(data, 1));
+    await expect(createUserUseCase.execute(data)).resolves.toBeTruthy();
+  });
+});
---
+++ api/src/useCases/user/createUser/create-user.usecase.ts
@@ -0,0 +1,50 @@
+import * as yup from "yup";
+import bcrypt from "bcrypt";
+import { User } from "@prisma/client";
+import { IUseCase, IUserRepository } from "../../../interfaces";
+import { CreateUserDto } from "./create-user.dto";
+import { IntegrityError } from "../../../errors";
+
+export class CreateUserUseCase implements IUseCase<CreateUserDto, User> {
+  constructor(private readonly userRepository: IUserRepository) {}
+
+  async validateCreateUserData(data: CreateUserDto): Promise<CreateUserDto> {
+    const createUserDataConstraints = yup.object().shape({
+      firstName: yup.string().required().min(3).max(30).trim(),
+      lastName: yup.string().required().min(3).max(50).trim(),
+      email: yup.string().required().email().lowercase().trim(),
+      password: yup.string().required().min(6).max(50),
+      isActive: yup.boolean().required(),
+      isVerified: yup.boolean().required(),
+    });
+    return createUserDataConstraints.validate(data);
+  }
+
+  async checkUserUniqueFields(data: CreateUserDto): Promise<true> {
+    const matchingUsers = await this.userRepository.findMany({
+      where: {
+        OR: { email: data.email },
+      },
+    });
+    matchingUsers.forEach((user) => {
+      if (user.email === data.email) {
+        throw new IntegrityError("Email already exists");
+      }
+    });
+    return true;
+  }
+
+  async hashPassword(password: string): Promise<string> {
+    return bcrypt.hash(password, 12);
+  }
+
+  async execute(args: CreateUserDto): Promise<User> {
+    const validatedData = await this.validateCreateUserData(args);
+    await this.checkUserUniqueFields(validatedData);
+    const passwordHash = await this.hashPassword(validatedData.password);
+    return this.userRepository.create({
+      ...validatedData,
+      password: passwordHash,
+    });
+  }
+}
