86d8aa1ee7a91509d9699551e274f0ae45467f4e
João Rodrigues <joao.devmail@gmail.com>
2022-07-01 12:26:44 -0300

    feat: me graphql query

--- api/src/graphql/queries/user.ts
+++ api/src/graphql/queries/user.ts
@@ -1,10 +1,25 @@
+import { AuthenticationError } from "apollo-server-express";
 import { extendType, idArg } from "nexus";
+import { UserNotFoundError } from "../../errors";
 import { getUsersUseCase, getUserUseCase } from "../../useCases/user";
 import { parsePaginationArgs } from "../../utils";
 
 export const userQueries = extendType({
   type: "Query",
   definition(t) {
+    t.field("me", {
+      type: "User",
+      async resolve(_root, _args, ctx) {
+        if (!ctx.user) {
+          throw new AuthenticationError("Not Authorised!");
+        }
+        const user = await getUserUseCase.execute({ id: ctx.user.id });
+        if (!user) {
+          throw new UserNotFoundError("User not found");
+        }
+        return user;
+      },
+    });
     t.nullable.field("user", {
       type: "User",
       args: {
