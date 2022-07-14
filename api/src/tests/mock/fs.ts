5bf13cd76411efe46249c56ea203d058b4dbb098
João Rodrigues <joao.devmail@gmail.com>
2022-07-14 11:14:12 -0300

    fix: fs module mock

--- api/src/tests/mock/fs.ts
+++ api/src/tests/mock/fs.ts
@@ -1,13 +1,3 @@
 import fs from "fs";
-import { mockDeep, mockReset, DeepMockProxy } from "jest-mock-extended";
 
-jest.mock("fs", () => ({
-  __esModule: true,
-  default: mockDeep<typeof fs>(),
-}));
-
-beforeEach(() => {
-  mockReset(fsMock);
-});
-
-export const fsMock = fs as unknown as DeepMockProxy<typeof fs>;
+export const fsMock = jest.createMockFromModule("fs") as jest.Mocked<typeof fs>;
